873bed0446715034c567774a002945f67b1b8e74
Anugrah Lambogo <lambogoai@gmail.com>
2020-03-09 00:05:54 -0500

    client origin

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://pawsibilities-v2.now.sh/',
 };
